c6a9419cba17e56c01ccf2c7c55cce985d11cbe7
Erick Díaz Saborio <ewdiazsa@gmail.com>
2019-10-24 07:28:55 -0600

    correct split of training data

--- split_training_dataset.py
+++ split_training_dataset.py
@@ -10,17 +10,19 @@ train_data_raw = h5py.File(DATA_PATH+'train.mat')
 
 x_train = train_data_raw['trainxdata']
 y_train = train_data_raw['traindata']
+
+x_train = np.transpose(x_train, (2, 1, 0)) # shape = (4400000,  4, 1000)
+y_train = np.transpose(y_train, (1, 0)) # shape = (4400000, 919)
+
 print("training data set [DONE]")
 
-
-for i,n in enumerate(range(0,1000,100)):
+split = 400000
+for i,n in enumerate(range(0,4400000,split)):
     fn = "x_train_part_{}".format(i)
     yfn = "y_train_part_{}".format(i)
     
-    print("\nSaving Part {} ....".format(i))
-    np.save(DATA_PATH+fn, x_train[n:n+100])
-    np.save(DATA_PATH+yfn, y_train[n:n+100])
+    print("\nSaving Part {} ....[{}:{}]".format(i, n, n+split))
+    np.save(DATA_PATH+fn, x_train[n:n+split])
+    np.save(DATA_PATH+yfn, y_train[n:n+split])
     print("{} [Done]".format(fn))
     print("{} [Done]".format(yfn))
-
-
